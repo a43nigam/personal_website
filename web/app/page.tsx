3df6135a34d3ecf6648505603a100ac246b8df2b
a3yu <74028906+a3yu@users.noreply.github.com>
2024-08-26 14:46:34 -0400

    Update page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -150,7 +150,10 @@ export default function Home() {
         </section>
 
         <section className="bg-white py-6">
-          <h2 className="text-md font-semibold mb-4 text-black">Graveyard</h2>
+          <span className="flex space-x-2 items-center mb-4">
+            <h2 className="text-md font-semibold text-black">Graveyard</h2>
+            <h2 className="italic text-xs">(Build quick, fail fast)</h2>
+          </span>
           <div className="space-y-2">
             <div
               className="flex justify-between items-center cursor-pointer border-b border-gray-200 pb-2"
