d9e83374b6ba964d9f9b66fdf545a71455cefc54
Anubhav Nigam <a43nigam@gmail.com>
2024-09-25 14:06:14 -0400

    Update page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -62,39 +62,16 @@ export default function Home() {
           </p>
         </section>
 
-{/*         <section className="bg-white py-6  border-gray-200">
-          <h2 className="text-md font-semibold mb-4">School</h2>
-            <div
-              className="flex justify-between items-center cursor-pointer border-b border-gray-200 pb-2"
-              onClick={() => handleWorkItemClick(0)}
-            >
-              <div className="flex items-center">
-                <img src="/cu.png" alt="Cornell" className="w-6 h-6 mr-2" />
-                <h3 className="text-sm text-black">Cornell - Applied & Engineering Physics (B.S.)</h3>
-              </div>
-              {expandedWorkIndex === 0 ? <FaChevronUp /> : <FaChevronDown />}
-            </div>
-            {expandedWorkIndex === 0 && (
-              <div className="text-sm transition-all duration-300 ease-in-out overflow-hidden">
-                <p className="text-gray-700">
-                  <span className="font-semibold">
-                    Coursework:
-                  </span>{" "}
-                  Lasers and Photonics, Spacecraft Propulsion, Nanofabrication and Electronic Characterization
-                </p>
-              </div>
-            )}
-        </section> */}
-
         <section className="bg-white py-6  border-gray-200">
           <h2 className="text-md font-semibold mb-4">Work</h2>
+          <div className="space-y-2">
             <div
               className="flex justify-between items-center cursor-pointer border-b border-gray-200 pb-2"
               onClick={() => handleWorkItemClick(0)}
             >
               <div className="flex items-center">
                 <img src="/cu.png" alt="Cornell" className="w-6 h-6 mr-2" />
-                <h3 className="text-sm text-black">Cornell - Undergraduate Researcher</h3>
+                <h3 className="text-sm text-black">Cornell - Undergrad Researcher</h3>
               </div>
               {expandedWorkIndex === 0 ? <FaChevronUp /> : <FaChevronDown />}
             </div>
@@ -102,7 +79,7 @@ export default function Home() {
               <div className="text-sm transition-all duration-300 ease-in-out overflow-hidden">
                 <p className="text-gray-700">
                   <span className="font-semibold">
-                    Studying superconducting superlattices.
+                    Studying superconducting superlattices
                   </span>{" "}
                   Work in Yasuda Lab to use van der Waals materials to fabricate/study low dimensional heterostructures - 
                   these have applications in non-volatile memory and quantum information science. See projects for more info.
@@ -115,7 +92,7 @@ export default function Home() {
             >
               <div className="flex items-center">
                 <img
-                  src="/af.jpg"
+                  src="/af.png"
                   alt="Space Force"
                   className="w-6 h-6 mr-2"
                 />
@@ -129,7 +106,7 @@ export default function Home() {
               <div className="text-sm transition-all duration-300 ease-in-out overflow-hidden">
                 <p className="text-gray-700">
                   <span className="font-semibold">
-                    Ionospheric plasma physics / Materials development.
+                    Ionospheric plasma physics / Materials development
                   </span>{" "}
                   Experimentally modelled ionospheric plasma behavior. Explored novel processes to 
                   develop materials relevant to the Space Force.
@@ -157,7 +134,7 @@ export default function Home() {
               <div className="text-sm transition-all duration-300 ease-in-out overflow-hidden">
                 <p className="text-gray-700">
                   <span className="font-semibold">
-                    Magnetic thrust and levitation.
+                    Magnetic thrust and levitation
                   </span>{" "}
                   Worked with the team to design, engineer, and implement a linear induction motor in our pod.
                   Currently working to engineer maglev.
