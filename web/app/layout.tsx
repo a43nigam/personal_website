5ff845378c59950d3900cab1688a3ba188791c40
Anubhav Nigam <a43nigam@gmail.com>
2024-09-25 13:50:20 -0400

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Aedin Yu",
+  title: "Anubhav Nigam",
   description: "My personal website",
 };
 
