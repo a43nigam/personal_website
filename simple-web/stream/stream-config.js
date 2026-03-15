b103eb77007b85812615b3c2052d53ff9d8a036a
Anubhav Nigam <a43nigam@gmail.com>
2026-03-15 01:46:24 -0400

    stream stuff

---
+++ simple-web/stream/stream-config.js
@@ -0,0 +1,7 @@
+window.STREAM_CONFIG = {
+  title: "Live Stream",
+  description: "Public viewer for the always-on stream. The player reconnects automatically when the feed drops.",
+  hlsUrl: "",
+  rtspUrl: "",
+  posterUrl: ""
+};
